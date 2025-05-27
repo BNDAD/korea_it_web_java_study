aa98ae0f9107564d702b0ce7a1efcb51411207ad
BNDAD <ral7057@naver.com>
2025-05-27 21:38:35 +0900

    Feat: Cating

---
+++ src/_25_Casting/Animal/Lion.java
@@ -0,0 +1,12 @@
+package _25_Casting.Animal;
+
+public class Lion extends Animal{
+    @Override
+    public void speak() {
+        System.out.println("아웅");
+    }
+    public void eatFood() {
+        System.out.println("음식을 먹는다.");
+    }
+}
+
---
+++ src/_25_Casting/Animal/Tiger.java
@@ -0,0 +1,12 @@
+package _25_Casting.Animal;
+
+public class Tiger extends Animal{
+    @Override
+    public void speak() {
+        System.out.println("어흥");
+    }
+    public void Dance() {
+        System.out.println("춤 춘다.");
+    }
+}
+
